9246fcfc904c37fe692be92132815043e824441a
Matthew Schroeter <schroeter.m83@gmail.com>
2020-02-25 14:13:19 -0600

    added acf to composer.json

---
+++ plugins/mattschroeter/mattschroeter.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Core Functionality
+Plugin URI: https://mattschroeter.me
+Description: Core functionality for mattschroeter.me
+Version: 1.0.0
+Author: Matt Schroeter
+Author URI: https://mattschroeter.me
+Text Domain: tfr
+Domain Path: /lang
+*/
